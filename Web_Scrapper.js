f5b73010386e5ba9b4c264b1160c0b5eaba73bc1
Bobman4u <bobgriffies@live.co.uk>
2016-11-14 02:55:53 +0000

    Web_Scrapper

--- Web_Scrapper.js
+++ Web_Scrapper.js
@@ -6,17 +6,21 @@ request(url, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body);
 
-    var name, phone, email, address;
+    var name, phone, email, addressStreet, addressLocal, addressPostal;
 
-    name = $("[h4].firstChild.data").html();
-    phone = $().html();
-    email = $().html();
-    address = $().html();
+    name = $('h4').html();
+    phone = $('span[itemprop="telephone"]').html();
+    email = $('span[itemprop="email"]').html();
+    addressStreet = $('span[itemprop="streetAddress"]').html();
+    addressLocal = $('span[itemprop="addressLocality"]').html();
+    addressPostal = $('span[itemprop="postalCode"]').html();
       
     console.log("Comapny Name; " + name);
     console.log("Phone; " + phone);
     console.log("Email; " + email);
-    console.log("Address; " + address);
+    console.log("Address; " + addressStreet);
+    console.log("         " + addressLocal);
+    console.log("         " + addressPostal);
   } else {
     console.log("We’ve encountered an error: " + error);
   }
